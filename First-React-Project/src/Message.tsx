d3e0656e8d3dde80aed085fc246f6c7f5ab06486
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-01 21:58:16 +0530

    Message.tsx

---
+++ First-React-Project/src/Message.tsx
@@ -0,0 +1,14 @@
+
+// PascalCasing style
+function Message(){
+    // JSX: JavaScript XML
+    const name = 'Chanku'
+    return (
+        <>
+            <h1>Hello World</h1>
+            <h1>Hello {name}</h1>
+        </>
+    )
+}
+
+export default Message;
