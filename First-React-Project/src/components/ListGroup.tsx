bea651abadb95a7b9d01625c0a47e219802d91a3
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-01 22:16:23 +0530

    more lists

--- First-React-Project/src/components/ListGroup.tsx
+++ First-React-Project/src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 function ListGroup() {
+  const items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+
+  items.map((item) => <li>{item}</li>);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
