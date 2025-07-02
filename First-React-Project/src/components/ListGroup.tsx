5f60f77ab7e241393be17b621bcc4f3fb4cb6428
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-07-02 11:32:46 +0530

    Click Events

--- First-React-Project/src/components/ListGroup.tsx
+++ First-React-Project/src/components/ListGroup.tsx
@@ -1,6 +1,11 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
   // items = [];
+
+  // Event Handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
   return (
     <>
@@ -9,7 +14,7 @@ function ListGroup() {
       {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
